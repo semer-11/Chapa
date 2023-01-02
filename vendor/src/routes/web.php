e4bc671f7682316fa93722ad1559659cd702818b
SEMREDIN NUREGBA <semernur11@gmail.com>
2023-01-02 23:27:46 +0300

    Test class added

--- vendor/src/routes/web.php
+++ vendor/src/routes/web.php
@@ -3,8 +3,12 @@
 use Chapa\Controller\Test;
 use Illuminate\Support\Facades\Route;
 
+if (env('APP_ENV') != 'production') {
 
-Route::get('/chapa/initialize', [Test::class, 'initialize']);
-Route::get('/chapa/verify/{ref}', [Test::class, 'verify']);
-Route::get('/chapa/verifyLatest', [Test::class, 'latest']);
-Route::get('/chapa/verifyTxById/{id}', [Test::class, 'verifyById']);
+    Route::get('/chapa/initialize', [Test::class, 'initialize']);
+    Route::get('/chapa/verify/{ref}', [Test::class, 'verify']);
+    Route::get('/chapa/verifyLatest', [Test::class, 'latest']);
+    Route::get('/chapa/verifyTxById/{id}', [Test::class, 'verifyById']);
+} else {
+    abort(404);
+}
