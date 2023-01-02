454a2115237c1db6e96b377bccefb20e0e3ba6c8
SEMREDIN NUREGBA <semernur11@gmail.com>
2023-01-02 19:46:01 +0300

    v1.0.0 Chapa SDK for Laravel

--- src/ChapaServiceProvider.php
+++ src/ChapaServiceProvider.php
@@ -12,14 +12,6 @@ class ChapaServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . './routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . './migrations');
-        $this->loadViewsFrom(__DIR__ . './views', 'chapa');
-
-
-
-
-        $this->publishes([
-            __DIR__ . './views' => resource_path('views/chapa'), 'views'
-        ]);
         $this->publishes([
             __DIR__ . './migrations/' => database_path('migrations')
         ], 'database');
@@ -29,6 +21,7 @@ class ChapaServiceProvider extends ServiceProvider
             ],
             'model'
         );
+
         $this->publishes(
             [
                 __DIR__ . './controllers/' => app_path('Http/Controllers/Chapa')
